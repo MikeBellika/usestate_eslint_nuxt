4c2a43bf8758b14c4b1ca93d03216419a34b7fce
Mike Bellika <mb@moxso.com>
2024-06-19 08:40:10 +0200

    initial commit

---
+++ app.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const myState = useState<boolean>("state", () => false);
+// This does not give any eslint errors
+if (myState) {
+  console.log("myState");
+}
+const myRef = ref(false);
+// This gives an eslint error
+if (myRef) {
+  console.log("myRef");
+}
+</script>
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
